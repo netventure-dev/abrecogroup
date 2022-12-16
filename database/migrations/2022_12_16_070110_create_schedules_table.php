0ab2311ae8b11366cf104a8dbd7169e6bb89e444
VIPIN KUMAR <vipin@netventure.in>
2022-12-16 13:01:59 +0530

    commit changes

--- database/migrations/2022_12_16_070110_create_schedules_table.php
+++ database/migrations/2022_12_16_070110_create_schedules_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateBundlesTable extends Migration
+class CreateSchedulesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,13 @@ class CreateBundlesTable extends Migration
      */
     public function up()
     {
-        Schema::create('bundles', function (Blueprint $table) {
+        Schema::create('schedules', function (Blueprint $table) {
             $table->bigIncrements('id', true);
-            $table->string('no_of_bundles');
-            $table->string('no_of_rods');
-            $table->integer('size_id')->unsigned();
-            $table->string('weight');
-            $table->string('rate')->nullable();
-            $table->string('status')->default(0);
+            $table->string('venue');
+            $table->text('topic');
+            $table->string('speakers')->nullable();
+            $table->date('schedule_date')->nullable();
+            $table->time('schedule_time')->nullable();
             $table->timestamps();
         });
     }
@@ -32,6 +31,6 @@ class CreateBundlesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('bundles');
+        Schema::dropIfExists('schedules');
     }
 }
