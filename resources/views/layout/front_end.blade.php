2168176870da621c1c1d89bef92a84834ca4c676
Aravind <aravind.netventure@gmail.com>
2023-01-12 15:51:49 +0530

    GTM Added

--- resources/views/layout/front_end.blade.php
+++ resources/views/layout/front_end.blade.php
@@ -22,9 +22,25 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">
     <link href="{{ asset('assets/front/css/style.css') }}" rel="stylesheet">
     @yield('css')
+    <!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-KJWG467');</script>
+    <!-- End Google Tag Manager -->
+    
 </head>
 
 <body>
+    GTM BODY
+___________________
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KJWG467"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+______________________________________________
+
     {{-- @dd($services); --}}
     <!-- HEADER -->
     <header class="fixed-top header_area">
