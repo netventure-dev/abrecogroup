1a14d26401dd07cd4ea46519b8a671f89ab1438b
Aravind <aravind.netventure@gmail.com>
2023-01-12 10:58:34 +0530

    404 page

--- app/Http/Controllers/ServiceController.php
+++ app/Http/Controllers/ServiceController.php
@@ -13,9 +13,10 @@ class ServiceController extends Controller
     public function index($slug)
     {
         $service = Service::where('slug',$slug)->where('status',1)->first();
+        if($service){
         $content = ServiceContent::where('service_id',$service->uuid)->get();
         $faqs = ServiceFaq::where('service_id',$service->uuid)->get();
-        if($service){
+       
             return view('service',with(['service' => $service,'content' => $content,'faqs' => $faqs]));
         }else{
             abort(404);
