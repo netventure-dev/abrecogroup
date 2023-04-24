10e3be2036167ecaf0cc713f982654e6ec6adbd1
teztri <66072368+sarannetventure@users.noreply.github.com>
2023-04-24 12:43:15 +0530

    changee

--- database/migrations/2022_12_28_043726_create_feedback_table.php
+++ database/migrations/2022_12_28_043726_create_feedback_table.php
@@ -18,7 +18,7 @@ class CreateFeedbackTable extends Migration
             $table->string('name');
             $table->string('email');
             $table->string('phone');
-            $table->text('message')->nullable();
+            $table->text('message');
             $table->timestamps();
         });
     }
--- database/migrations/2022_12_28_065756_create_contacts_table.php
+++ database/migrations/2022_12_28_065756_create_contacts_table.php
@@ -18,7 +18,7 @@ class CreateContactsTable extends Migration
             $table->string('name');
             $table->string('email');
             $table->string('phone');
-            $table->text('message')->nullable();
+            $table->text('message');
             $table->timestamps();
         });
     }
