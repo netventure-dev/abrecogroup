86b3186a74e86945ab01dee421094e6c52f78c22
Aravind <aravind.netventure@gmail.com>
2023-04-26 15:38:38 +0530

    styling

--- app/Http/Controllers/Api/IndustryController.php
+++ app/Http/Controllers/Api/IndustryController.php
@@ -14,24 +14,28 @@ class IndustryController extends Controller
 
     public function index()
     {
-        $data['industries'] = Industry::where('status',1)->select('id','uuid','name')->orderBy('created_at','desc')->get();
+        $data['industries'] = Industry::where('status', 1)->select('id', 'uuid', 'name')->orderBy('created_at', 'desc')->get();
 
         if (!empty($data)) {
             return response()->json(['code' => 200, 'message' => 'Successful', 'data' => $data], $this->successStatus);
-          }
+        }
         return response()->json(['code' => 404, 'message' => 'No Data Available', 'data' => $data], $this->successStatus);
     }
 
     public function details($uuid)
     {
-        $data['industry'] = Industry::where('uuid',$uuid)->where('status',1) ->with(['content' => function ($query) {
-            $query->where('status', 1)->select('id','uuid','industries_id','title','description','order','image','button_title','button_link')->orderBy('order','ASC');
-        }])->first();
+        $data['industry'] = Industry::where('uuid', $uuid)->where('status', 1)
+            ->select('id', 'uuid', 'name')
+            ->with(['content' => function ($query) {
+                $query->where('status', 1)
+                    ->select('id', 'uuid', 'industries_id', 'title', 'description', 'order', 'image', 'button_title', 'button_link')
+                    ->orderBy('order', 'ASC');
+            }])->first();
         // $data['industry']['industry_content'] =  $data['industry']->contents->where('status',1);
 
         if (!empty($data)) {
             return response()->json(['code' => 200, 'message' => 'Successful', 'data' => $data], $this->successStatus);
-          }
+        }
         return response()->json(['code' => 404, 'message' => 'No Data Available', 'data' => $data], $this->successStatus);
     }
 }
