2bf3967261b269eb85f750a95fe7125b12cceea8
VIPIN KUMAR <vipin@netventure.in>
2023-05-17 16:02:58 +0530

    changes

--- app/Http/Controllers/Api/ServiceApiController.php
+++ app/Http/Controllers/Api/ServiceApiController.php
@@ -61,8 +61,8 @@ class ServiceApiController extends Controller
                                 ->with(['innerservices'=> function($query) {
                                     $query->select('id','sub_service_id','subservice as subservice_name', 'uuid', 'name','cover_image','logo','slug','cover_description','title','description')->where('status',1);
                                 },'services'=> function($query) {
-                                    $query->select('name','id','slug')->where('status',1);
-                                },]
+                                    $query->select('name as service_name','slug as service_slug')->where('status',1);
+                                },])
                                 ->where('uuid', $sub_id)
                                 ->where('service_id', $uuid)
                                 ->where('status', 1)
