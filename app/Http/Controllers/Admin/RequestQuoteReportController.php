8f91e97a0420143126fe8566211179d4c9508d14
VIPIN KUMAR <vipin@netventure.in>
2023-06-15 11:46:02 +0530

    bug in name removed

--- app/Http/Controllers/Admin/RequestQuoteReportController.php
+++ app/Http/Controllers/Admin/RequestQuoteReportController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers\Admin;
 
-use App\DataTables\RequestQuoteReportDataTable;
+use App\DataTables\Admin\RequestQuoteReportDataTable;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
