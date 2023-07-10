187defeb5b82aa8019a2b3adb9806a7763afa7ad
VIPIN KUMAR <vipin@netventure.in>
2023-07-10 19:47:10 +0530

    changes

--- resources/views/admin/layout/backend.blade.php
+++ resources/views/admin/layout/backend.blade.php
@@ -45,20 +45,20 @@
                     <div class="navbar-brand-box">
                         <a href="index.html" class="logo logo-dark">
                             <span class="logo-sm">
-                                <img src="{{asset('storage/'.@$logo)}}" alt="" height="22">
+                                <img src="{{asset('assets/images/logo.jpg')}}" alt="" height="50">
                             </span>
                             <span class="logo-lg">
-                                <img src="{{asset('storage/'.@$logo)}}" alt="" height="17">
+                                <img src="{{asset('assets/images/logo.jpg')}}" alt="" height="50">
                             </span>
                         </a>
 
                         <a href="index.html" class="logo logo-light">
-                            {{--  <span class="logo-sm">
-                                <img src="{{ asset('assets/images/logo-light.svg')}}" alt="" height="22">
-                            </span>  --}}
-                            {{--  <span class="logo-lg">
-                                <img src="{{ asset('assets/images/logo-light.png')}}" alt="" height="19">
-                            </span>  --}}
+                             <span class="logo-sm">
+                                <img src="{{asset('assets/images/logo.jpg')}}" alt="" height="50">
+                            </span>
+                             <span class="logo-lg">
+                                <img src="{{ asset('assets/images/logo.jpg')}}" alt="" height="50">
+                            </span> 
                         </a>
                     </div>
 
