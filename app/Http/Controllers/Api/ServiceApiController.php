14f2fca0b9b635f09706b48caa1f54a9361df01d
VIPIN KUMAR <vipin@netventure.in>
2023-07-18 10:52:43 +0530

    changes

--- app/Http/Controllers/Api/ServiceApiController.php
+++ app/Http/Controllers/Api/ServiceApiController.php
@@ -42,7 +42,8 @@ class ServiceApiController extends Controller
                                 }, 'contents' => function($query) {
                                     $query->select('id','service_id', 'uuid', 'title','description','order','image','mobile_image')->where('status',1);
                                 },'subservices' => function($query) {
-                                    $query->select('id','service_id','service as service_name','service_slug', 'uuid', 'name','cover_image','logo','slug','cover_description','title','description')->where('status',1);
+                                    $query->select('id','service_id','service as service_name','service_slug', 'uuid', 'name','cover_image','logo','slug','cover_description','title','description');
+                                    // ->where('status',1);
                                 },'subservices.innerservices'=> function($query) {
                                     $query->select('id','service_id','sub_service_id', 'service_name','service_slug','subservice as subservice_name', 'sub_service_slug','uuid', 'name','cover_image','logo','slug','cover_description','title','description')->where('status',1);
                                 },'contents.extra_contents'])
