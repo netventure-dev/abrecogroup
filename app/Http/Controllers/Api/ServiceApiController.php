56ec176d6c634ae567baeafcd49a7f044d933f23
VIPIN KUMAR <vipin@netventure.in>
2023-09-12 14:42:43 +0530

    update

--- app/Http/Controllers/Api/ServiceApiController.php
+++ app/Http/Controllers/Api/ServiceApiController.php
@@ -67,7 +67,7 @@ class ServiceApiController extends Controller
                                 },'contents' => function($query) {
                                     $query->select('id','sub_service_id', 'uuid', 'title','sub_title','description','order','image','mobile_image')->where('status',1);
                                 }, 'casestudy' => function($query) {
-                                    $query->select('id','service_id','service_slug as service_id_slug','sub_service_slug as sub_service_id_slug','inner_service_slug as inner_service_id_slug', 'uuid', 'title','subtitle','image1','content','image2')->where('status',1);
+                                    $query->select('id','service_id','sub_service_id', 'uuid', 'title','subtitle','image1','content','image2')->where('status',1);
                                 },'contents.extra_contents'])
                                 ->where('slug', $sub_id)
                                 ->where('service_slug', $uuid)
@@ -85,7 +85,7 @@ class ServiceApiController extends Controller
         ->with(['contents' => function($query){
         $query->select('id', 'inner_service_id','uuid', 'title','sub_title', 'description', 'order','image')->where('status',1);
                                 }, 'casestudy' => function($query) {
-                                    $query->select('id','service_id','service_slug as service_id_slug','sub_service_slug as sub_service_id_slug','inner_service_slug as inner_service_id_slug', 'uuid', 'title','subtitle','image1','content','image2')->where('status',1);
+                                    $query->select('id','service_id','sub_service_id', 'inner_service_id','uuid', 'title','subtitle','image1','content','image2')->where('status',1);
                                 },'contents.extra_contents'])
                                 ->where('slug', $sub_id)
                                 ->where('sub_service_slug', $uuid)
