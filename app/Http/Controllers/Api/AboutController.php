6dc2f893584d829c08cb5af9d84a7d2570f48dc1
teztri <66072368+sarannetventure@users.noreply.github.com>
2023-09-21 10:34:13 +0530

    api update

--- app/Http/Controllers/Api/AboutController.php
+++ app/Http/Controllers/Api/AboutController.php
@@ -12,15 +12,32 @@ class AboutController extends Controller
 {
     public $successStatus = 200;
     public $failedStatus = 400;
- 
+
     public function index()
     {
-        $data['about'] = AboutUs::select('cover_title','cover_content','banner_image','content_image','content','link','seo_title','seo_description','seo_keywords')->first();
-        $data['about_list'] = AboutUsList::select('id','title','content','icon','link')->where('status',1)->get();
-        $data['mission_vision']=MissionVision::select('id','uuid','title','description','image','mobile_image')->where('status',1)->get();
+        $data['about'] = AboutUs::select('cover_title', 'cover_content', 'banner_image', 'content_image', 'content', 'link', 'seo_title', 'seo_description', 'seo_keywords')->first();
+        $data['about_list'] = AboutUsList::select('id', 'title', 'content', 'icon', 'link')->where('status', 1)->get();
+        $data['mission_vision'] = MissionVision::select('id', 'uuid', 'title', 'description', 'image', 'mobile_image','status')->get()
+       
+        ->map(function ($item) {
+            // dd($item);
+            if ($item->status == 0) {
+                return [
+                    'id' => null,
+                    'title' => null,
+                    'content' => null,
+                    'icon' => null,
+                    'link' => null,
+                    
+                ];
+            } else {
+                return $item;
+            }
+        });
+
         if (!empty($data)) {
             return response()->json(['code' => 200, 'message' => 'Successful', 'data' => $data], $this->successStatus);
-         }
-         return response()->json(['code' => 404, 'message' => 'No Data Available', 'data' => $data], $this->failedStatus);
+        }
+        return response()->json(['code' => 404, 'message' => 'No Data Available', 'data' => $data], $this->failedStatus);
     }
 }
