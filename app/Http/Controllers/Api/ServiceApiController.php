332b5a05de24016e36a2afb640150f315d8fc45d
teztri <66072368+sarannetventure@users.noreply.github.com>
2023-09-22 12:56:44 +0530

    change

--- app/Http/Controllers/Api/ServiceApiController.php
+++ app/Http/Controllers/Api/ServiceApiController.php
@@ -81,9 +81,9 @@ class ServiceApiController extends Controller
     }
     public function sub_services($uuid, $sub_id)
     {
-        $data['sub_services'] = SubService::select('id', 'service_id', 'service as service_name', 'service_slug', 'uuid', 'custom_url', 'name', 'cover_image', 'logo', 'slug', 'cover_description', 'title', 'description', 'status')
+        $data['sub_services'] = SubService::select('id', 'service_id', 'service as service_name', 'service_slug', 'uuid', 'custom_url', 'name', 'cover_image', 'logo', 'slug', 'cover_description', 'title', 'description', 'status', 'seo_title', 'seo_description', 'seo_keywords')
             ->with(['innerservices' => function ($query) {
-                $query->select('id', 'service_id', 'sub_service_id', 'service_name', 'service_slug', 'subservice as subservice_name', 'sub_service_slug', 'uuid', 'name', 'cover_image', 'logo', 'slug', 'cover_description', 'title', 'description', 'seo_title', 'seo_description', 'seo_keywords')->where('status', 1);
+                $query->select('id', 'service_id', 'sub_service_id', 'service_name', 'service_slug', 'subservice as subservice_name', 'sub_service_slug', 'uuid', 'name', 'cover_image', 'logo', 'slug', 'cover_description', 'title', 'description', )->where('status', 1);
             }, 'contents' => function ($query) {
                 $query->select('id', 'sub_service_id', 'uuid', 'title', 'sub_title', 'description', 'order', 'image', 'mobile_image','status');
             }, 'casestudy' => function ($query) {
@@ -119,7 +119,7 @@ class ServiceApiController extends Controller
     }
     public function inner_services($id, $uuid, $sub_id)
     {
-        $data['inner_services'] = InnerService::select('id', 'service_id', 'sub_service_id', 'service_name', 'service_slug', 'custom_url', 'subservice as subservice_name', 'sub_service_slug', 'uuid', 'name', 'cover_image', 'logo', 'slug', 'cover_description', 'title', 'description', 'status')
+        $data['inner_services'] = InnerService::select('id', 'service_id', 'sub_service_id', 'service_name', 'service_slug', 'custom_url', 'subservice as subservice_name', 'sub_service_slug', 'uuid', 'name', 'cover_image', 'logo', 'slug', 'cover_description', 'title', 'description', 'status', 'seo_title', 'seo_description', 'seo_keywords')
             ->with(['contents' => function ($query) {
                 $query->select('id', 'inner_service_id', 'uuid', 'title', 'sub_title', 'description', 'order', 'image')->where('status', 1);
             }, 'casestudy' => function ($query) {
