4954bd9ba0db27e26d3391d6097bfc41a5ed38df
teztri <66072368+sarannetventure@users.noreply.github.com>
2023-10-31 11:10:54 +0530

    career

--- app/Http/Controllers/Api/CareerApiController.php
+++ app/Http/Controllers/Api/CareerApiController.php
@@ -13,13 +13,15 @@ use Illuminate\Support\Facades\Notification;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Validator;
 
 class CareerApiController extends Controller
 {
     public $successStatus = 200;
     public $failedStatus = 400;
 
-    public function index(Request $request){
+    public function index(Request $request)
+    {
         $data['career'] = CareerOpening::select('uuid', 'position', 'description', 'experience', 'seo_title', 'seo_description', 'seo_keywords')->where('status', 1)->get();
         if (!empty($data)) {
             return response()->json(['code' => 200, 'message' => 'Successful', 'data' => $data], $this->successStatus);
@@ -29,42 +31,95 @@ class CareerApiController extends Controller
     public function store(Request $request)
     {
         //return 2;
-        $validated = $request->validate(
-            [
-                'name' => 'required|max:255',
-                'email' => 'required|email|max:255',
-                'phone' => 'required|regex:/^([0-9\s\-\+\(\)]*)$/|min:10',
-                'message' => 'required',
-                'position' => 'required',
-                'resume' => 'required|mimes:pdf|max:5000',
-                // 'g-recaptcha-response' => 'required|captcha',
+        //     $validated = $request->validate(
+        //         [
+        //             'name' => 'required|max:255',
+        //             'email' => 'required|email|max:255',
+        //             'phone' => 'required|regex:/^([0-9\s\-\+\(\)]*)$/|min:10',
+        //             'message' => 'required',
+        //             'position' => 'required',
+        //             'resume' => 'required|mimes:pdf|max:5000',
+        //             // 'g-recaptcha-response' => 'required|captcha',
 
-            ],  [
-                'resume.max' => 'The resume may not be greater than 2 mb.',
-            ]);
-        // return 1;
+        //         ],  [
+        //             'resume.max' => 'The resume may not be greater than 2 mb.',
+        //         ]);
+        //     // return 1;
+        //     $data = new Career();
+        //     $data->uuid = (string) Str::uuid();
+        //     $data->name = $validated['name'];
+        //     $data->email = $validated['email'];
+        //     $data->phone = $validated['phone'];
+        //     $data->position = $validated['position'];
+        //     if ($request->hasFile('resume')) {
+        //         $path =  $request->file('resume')->storeAs('media/career/file', $validated['resume']->getClientOriginalName(), 'public');
+        //         $data->resume = $path;
+        //     }
+        //     $data->message = $validated['message'];
+        //     // dd($data);
+        //     $res = $data->save();
+        //     // return view('contact_us.show');
+
+        //     if ($res) {
+        //         $admin = Admin::first();
+        //         $details['name'] = $validated['name'];
+        //         $details['email'] = $validated['email'];
+        //         $details['phone'] = $validated['phone'];
+        //         $details['position'] = $validated['position'];
+        //         $details['message'] = $validated['message'];
+        //         $details['resume'] = $data->resume;
+        //         $details['admin_name'] = $admin->name;
+        //         Notification::send($admin, new CareerAdminNotification($details));
+        //         Notification::route('mail', $details['email'])->notify(new CareerNotification($details));
+        //         return response()->json(['code' => 200, 'message' => 'Successful', 'data' => $details], $this->successStatus);
+        //     } else {
+        //         return response()->json(['code' => 404, 'message' => 'Failed to submit data', 'data' => $details], $this->failedStatus);
+        //     }
+        // }
+
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|max:255',
+            'email' => 'required|email|max:255',
+            'phone' => 'required|regex:/^([0-9\s\-\+\(\)]*)$/|min:10',
+            'message' => 'required',
+            'position' => 'required',
+            'resume' => 'required|mimes:pdf|max:2048'
+
+        ], [
+            'resume.max' => 'The resume may not be greater than 2 mb.',
+        ]);
+
+        if ($validator->fails()) {
+            $response = [
+                'success' => false,
+                'errors' => $validator->errors(),
+                'input' => request()->all(),
+            ];
+
+            return response()->json($response); 
+        }
+
         $data = new Career();
         $data->uuid = (string) Str::uuid();
-        $data->name = $validated['name'];
-        $data->email = $validated['email'];
-        $data->phone = $validated['phone'];
-        $data->position = $validated['position'];
+        $data->name = $request['name'];
+        $data->email = $request['email'];
+        $data->phone = $request['phone'];
+        $data->position = $request['position'];
         if ($request->hasFile('resume')) {
-            $path =  $request->file('resume')->storeAs('media/career/file', $validated['resume']->getClientOriginalName(), 'public');
+            $path =  $request->file('resume')->storeAs('media/career/file', $request['resume']->getClientOriginalName(), 'public');
             $data->resume = $path;
         }
-        $data->message = $validated['message'];
-        // dd($data);
+        $data->message = $request['message'];
+
         $res = $data->save();
-        // return view('contact_us.show');
 
         if ($res) {
             $admin = Admin::first();
-            $details['name'] = $validated['name'];
-            $details['email'] = $validated['email'];
-            $details['phone'] = $validated['phone'];
-            $details['position'] = $validated['position'];
-            $details['message'] = $validated['message'];
+            $details['name'] = $request['name'];
+            $details['email'] = $request['email'];
+            $details['phone'] = $request['phone'];
+            $details['position'] = $request['position'];
+            $details['message'] = $request['message'];
             $details['resume'] = $data->resume;
             $details['admin_name'] = $admin->name;
             Notification::send($admin, new CareerAdminNotification($details));
