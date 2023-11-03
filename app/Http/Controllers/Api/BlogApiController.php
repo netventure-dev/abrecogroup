7349572a793117b951326e973d26e56baa43a618
vipin <vipin.netventure@gmail.com>
2023-11-03 11:56:32 +0530

    keywords updated

--- app/Http/Controllers/Api/BlogApiController.php
+++ app/Http/Controllers/Api/BlogApiController.php
@@ -16,7 +16,7 @@ class BlogApiController extends Controller
    public function index()
    {
         $data['blog'] = Blog::select('uuid', 'title','canonical_tag', 'description', 'image', 'status', 'seo_title', 'seo_description', 'seo_keyword')->first();
-        $data['blogLists'] = Bloglist::select('uuid', 'title','canonical_tag', 'description', 'image', 'slug')->where('status', 1)->get();
+        $data['blogLists'] = Bloglist::select('uuid', 'title','canonical_tag', 'description', 'image', 'slug', 'seo_title', 'seo_description', 'seo_keywords')->where('status', 1)->get();
         if (!empty($data)) {
             return response()->json(['code' => 200, 'message' => 'Successful', 'data' => $data], $this->successStatus);
         }
@@ -25,7 +25,7 @@ class BlogApiController extends Controller
 
    public function details($uuid)
    {
-        $data['blogLists'] = Bloglist::select('uuid', 'title','canonical_tag', 'description', 'image', 'slug')
+        $data['blogLists'] = Bloglist::select('uuid', 'title','canonical_tag', 'description', 'image', 'slug', 'seo_title', 'seo_description', 'seo_keywords')
                             ->where('slug', $uuid)
                             ->where('status', 1)
                             ->first();
