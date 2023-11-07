059c574ad9359c049e7df2d976d2124da2c527f6
vipin <vipin.netventure@gmail.com>
2023-11-07 15:21:54 +0530

    changes

--- app/Http/Controllers/Api/SchemaApiController.php
+++ app/Http/Controllers/Api/SchemaApiController.php
@@ -3,20 +3,16 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use App\Models\SchemaMarkup;
 use Illuminate\Http\Request;
+use App\Models\Schema;
 
 class SchemaApiController extends Controller
 {
-    public $successStatus = 200;
-    public $failedStatus = 400;
-    public function schema($slug)
+    public function schema()
     {
-        $data['schema'] = SchemaMarkup::select('id', 'uuid', 'title', 'route_name', 'description', 'status')->where('route_name',$slug)->first();
-
+        $data['schema'] = Schema::select('id','uuid','title','route_name','description')->first();
         if (!empty($data)) {
             return response()->json(['code' => 200, 'message' => 'Successful', 'data' => $data], $this->successStatus);
         }
         return response()->json(['code' => 404, 'message' => 'No Data Available', 'data' => $data], $this->failedStatus);
     }
-}
