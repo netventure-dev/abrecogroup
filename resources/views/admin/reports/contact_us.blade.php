6c579e3d502f7a698e834814798a89281d86a9a3
teztri <66072368+sarannetventure@users.noreply.github.com>
2023-11-14 14:06:28 +0530

    A3-15

--- resources/views/admin/reports/contact_us.blade.php
+++ resources/views/admin/reports/contact_us.blade.php
@@ -16,7 +16,7 @@
     <div class="row">
         <div class="col-12">
             <div class="page-title-box d-sm-flex align-items-center justify-content-between">
-                <h4 class="mb-sm-0 font-size-18">{{ __('Enquiry Reports') }}</h4>
+                <h4 class="mb-sm-0 font-size-18">{{ __('Contact Enquiry Reports') }}</h4>
                 <div class="page-title-right">
                     {{-- @can('create', App\Models\Admin::class) --}}
                     {{-- @endcan --}}
