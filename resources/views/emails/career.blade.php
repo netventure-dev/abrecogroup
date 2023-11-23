45e55e3381f94c9a3af85c761f243d4b766b496f
vipin <vipin.netventure@gmail.com>
2023-11-23 14:12:00 +0530

    logo in email error fixed

--- resources/views/emails/career.blade.php
+++ resources/views/emails/career.blade.php
@@ -3,7 +3,7 @@
 <body>
     <div
         style="font-size: 18px;background-color: #060332;text-align: center;max-width: 540px;max-height:200px;margin: auto;padding: 30px 0;">
-        <h2 style="color: #ffffff;font-size: 30px;font-weight: 800;"><img src="{{ asset('assets/images/logo.jpg') }}">
+        <h2 style="color: #ffffff;font-size: 30px;font-weight: 800;"><img src="https://backend.react.nvds.in/storage/media/general/image/a3logo-sm.webp">
         </h2>
     </div>
     <div
