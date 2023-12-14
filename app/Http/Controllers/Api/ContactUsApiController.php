637c119502805609172a06c4ec88185e1c74fec7
Arjun <arjun.netventure@gmail.com>
2023-12-14 14:38:05 +0530

    change in the validation message in recaptcha

--- app/Http/Controllers/Api/ContactUsApiController.php
+++ app/Http/Controllers/Api/ContactUsApiController.php
@@ -39,49 +39,6 @@ class ContactUsApiController extends Controller
     }
     public function store(Request $request)
     {
-        // //return 2;
-        // $validated = $request->validate([
-        //     'name' => 'required|max:255',
-        //     'email' => 'required|email|max:255',
-        //     'phone' => 'required|regex:/^([0-9\s\-\+\(\)]*)$/|min:10',
-        //     'message' => 'required',
-        //     'organization' => 'required',
-        //     'job' => 'required',
-        //     'reason' => 'required',
-        //     'refer' => 'required',
-        //     // 'g-recaptcha-response' => 'required|captcha',
-        // ]);
-        // // return 1;
-        // $data = new Contact();
-        // $data->name = $validated['name'];
-        // $data->email = $validated['email'];
-        // $data->phone = $validated['phone'];
-        // $data->organization = $validated['organization'];
-        // $data->job = $validated['job'];
-        // $data->reason = $validated['reason'];
-        // $data->refer = $validated['refer'];
-        // $data->message = $validated['message'];
-        // // dd($data);
-        // $res = $data->save();
-        // // return view('contact_us.show');
-
-        // if ($res) {
-        //     $admin = Admin::first();
-        //     $details['name'] = $validated['name'];
-        //     $details['email'] = $validated['email'];
-        //     $details['phone'] = $validated['phone'];
-        //     $details['organization'] = $validated['organization'];
-        //     $details['job'] = $validated['job'];
-        //     $details['refer'] = $validated['refer'];
-        //     $details['reason'] = $validated['reason'];
-        //     $details['message'] = $validated['message'];
-        //     $details['admin_name'] = $admin->name;
-        //     Notification::send($admin, new ContactNotification($details));
-        //     Notification::route('mail', $details['email'])->notify(new ContactusNotification($details));
-        //     return response()->json(['code' => 200, 'message' => 'Successful', 'data' => $details], $this->successStatus);
-        // } else {
-        //     return response()->json(['code' => 404, 'message' => 'Failed to submit data', 'data' => $details], $this->failedStatus);
-        // }
         $validator = Validator::make($request->all(), [
             'name' => 'required|max:255',
             'email' => 'required|email|max:255',
@@ -97,6 +54,8 @@ class ContactUsApiController extends Controller
         ], [
             'phone.min' => 'The phone must be at least 7 characters.',
             'phone.max' => 'The phone must be Maximum 16 characters.',
+            'g-recaptcha-response.required' => 'The RecaptchaToken is required',
+
 
 
         ]);
