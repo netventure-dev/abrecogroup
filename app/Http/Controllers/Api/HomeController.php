b6e0bbfb1ac4f6528e017948f82767ea2b8a3874
vipin <vipin.netventure@gmail.com>
2024-01-04 15:13:12 +0530

    changes

--- app/Http/Controllers/Api/HomeController.php
+++ app/Http/Controllers/Api/HomeController.php
@@ -40,7 +40,7 @@ class HomeController extends Controller
         // $data['blog']=Blog::select('uuid','title','description','image')->where('status',1)->get();     
         $data['blogLists'] = Bloglist::select('uuid', 'title','canonical_tag', 'description', 'image', 'slug')->where('status', 1)->get();
         $data['testimonials'] = Testimonial::select('uuid', 'title', 'position', 'description', 'image')->where('status', 1)->get();
-        $data['testimonial-settings'] = TestimonialSetting::select('uuid', 'title', 'logo')->first();
+        $data['testimonial-settings'] = TestimonialSetting::select('uuid', 'title', 'logo')->get();
         $data['all_sections'] = Section::with(['contents' => function ($query) {
                                 $query->select('uuid', 'section_id', 'title', 'icon', 'icon_content', 'button_title', 'button_link', 'order','status')->where('status', 1);
                             }])
